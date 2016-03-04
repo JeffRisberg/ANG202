69663e5cb9f5ddd52ec5a2842af855c29f3a57d8
JeffRisberg <jrisberg@aol.com>
2016-03-04 11:52:47 -0800

    converted to use router plugin, and material design css library

--- app/campaigns.component.ts
+++ app/campaigns.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
 import {Campaign} from './campaign';
 import {CampaignService} from './campaign.service';
 
--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,10 +1,30 @@
 import {Component, OnInit} from 'angular2/core';
 import {Router, Location} from 'angular2/router';
 
+import {Campaign} from './campaign';
+import {CampaignService} from './campaign.service';
+
 @Component({
     selector: 'my-dashboard',
     templateUrl: 'app/dashboard.component.html',
+    styleUrls: [ 'app/dashboard.component.css']
 })
+export class DashboardComponent implements OnInit {
 
-export class DashboardComponent {
+    campaigns:Campaign[] = [];
+
+    constructor(
+        private _router: Router,
+        private _campaignService: CampaignService) {
+    }
+
+    ngOnInit() {
+        this._campaignService.getCampaigns()
+            .then(campaigns => this.campaigns = campaigns.slice(1, 4));
+    }
+
+    gotoDetail(campaign: Campaign) {
+        let link = ['CampaignDetail', { id: campaign.id }];
+        this._router.navigate(link);
+    }
 }
